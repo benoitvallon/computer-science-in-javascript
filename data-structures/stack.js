61835ba9f518d58158d32f97bff397d7fc9a545e
Andric Villanueva <contact@andricvillanueva.com>
2017-05-10 21:58:37 -0400

    Moving things around

--- data-structures/stack.js
+++ data-structures/stack.js
@@ -1,41 +1,31 @@
-class Stack {
+export default class StackDS {
   constructor() {
     this.stack = [];
   }
 
-  push(value) {
-    this.stack.push(value);
+  getStack = () => (
+    this.stack
+  )
+
+  push = (value) => {
+    this.stack.push(value)
+
+    return this
   }
 
-  pop() {
-    return this.stack.pop();
-  }
+  pop = () => (
+    this.stack.pop()
+  )
 
-  peek() {
-    return this.stack[this.stack.length - 1];
-  }
+  peek = () => (
+    this.stack[this.length() - 1]
+  )
 
-  length() {
-    return this.stack.length;
-  }
+  length = () => (
+    this.stack.length
+  )
 
-  print() {
-    console.log(this.stack.join(' '));
-  }
+  print = () => (
+    this.stack.join(' ')
+  )
 }
-
-const stack = new Stack();
-stack.push(1);
-stack.push(2);
-stack.push(3);
-stack.print(); // => 1 2 3
-console.log('length is 3:', stack.length()); // => 3
-console.log('peek is 3:', stack.peek()); // => 3
-console.log('pop is 3:', stack.pop()); // => 3
-stack.print(); // => 1 2
-console.log('pop is 2:', stack.pop());  // => 2
-console.log('length is 1:', stack.length()); // => 1
-console.log('pop is 1:', stack.pop()); // => 1
-stack.print(); // => ''
-console.log('peek is undefined:', stack.peek()); // => undefined
-console.log('pop is undefined:', stack.pop()); // => undefined
---
+++ tests/stack.js
@@ -0,0 +1,42 @@
+import chai from 'chai'
+import StackDS from '../data-structures/stack'
+
+chai.should()
+const expect = chai.expect
+
+describe('Stack Data Structure', () => {
+  it('add() 3 find length()', (done) => {
+    const stack = new StackDS()
+    stack.push(1).push(2).push(3)
+
+    // Assertions
+    stack.length().should.equal(3)
+    stack.getStack().should.deep.equal([1, 2, 3])
+    stack.print().should.equal('1 2 3')
+
+    done()
+  })
+
+  it('add() 3 then peek() then add another and peek() again', (done) => {
+    const stack = new StackDS()
+    stack.push(1).push(2).push(3)
+
+    // Assertions
+    stack.peek().should.equal(3)
+    stack.push(2).peek().should.equal(2)
+
+    done()
+  })
+
+  it('add() 3 and pop()', (done) => {
+    const stack = new StackDS()
+    stack.push(1).push(2).push(3).pop()
+
+    // Assertions
+    stack.getStack().should.deep.equal([1, 2])
+    stack.push(1).getStack().should.deep.equal([1, 2, 1])
+    stack.length().should.equal(3)
+
+    done()
+  })
+})
