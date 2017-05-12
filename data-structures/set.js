df104a2204ed0211832de6e5be7f688bbaaee099
Andric Villanueva <contact@andricvillanueva.com>
2017-05-11 22:18:34 -0400

    Adding doc headers

--- data-structures/set.js
+++ data-structures/set.js
@@ -16,14 +16,11 @@ export default class SetDS {
     this.numberOfValues = 0
   }
 
-  getSet = () => (
-    this.values
-  )
-
   /**
    * Add a value to the set whilst insuring uniqueness.
    *
    * @param {mixed} value [description]
+   * @return {SetDS} Myself so we can keep this chain gang going.
    */
   add = (value) => {
     if (this.values.indexOf(value) < 0) {
@@ -34,6 +31,12 @@ export default class SetDS {
     return this
   }
 
+  /**
+   * Remove an item from the array.
+   *
+   * @param {mixed} value The value to be removed.
+   * @return {SetDS} Myself so we can keep this chain gang going.
+   */
   remove = (value) => {
     const index = this.values.indexOf(value)
 
@@ -46,6 +49,16 @@ export default class SetDS {
     return this
   }
 
+  /**
+   * Perform a union of two sets.
+   *
+   * A union is, essentially, just combining both sets. There are mutltiple ways
+   * to accomplish this task. Here I chose to use two sets instead of creating
+   * one large array and then removing duplicates.
+   *
+   * @param {SetDS} set A SetDS object
+   * @return {SetDS}
+   */
   union = (set) => {
     const newSet = new Set()
 
@@ -60,6 +73,16 @@ export default class SetDS {
     return newSet
   }
 
+  /**
+   * Find the intersection of two sets.
+   *
+   * To do this we want to compare each item in our current set (this.values) to
+   * the incomming set and if they are the same save it to a new set and return
+   * that intersection.
+   *
+   * @param {SetDS} set A SetDS object to compare against.
+   * @return {SetDS} The intersection
+   */
   intersect = (set) => {
     const intersection = new SetDS()
 
@@ -72,6 +95,14 @@ export default class SetDS {
     return intersection
   }
 
+  /**
+   * Find the difference between this set and the paramater set.
+   *
+   * Very similar to intersect but this time it is the opposite.
+   *
+   * @param {SetDS} set A SetDS object to compare against.
+   * @return {SetDS} The difference.
+   */
   difference = (set) => {
     const newSet = new SetDS()
 
@@ -84,6 +115,18 @@ export default class SetDS {
     return newSet
   }
 
+  /**
+   * Determine if the passed in set is a subset of me.
+   *
+   * This is different from the above two methods. This time we will use and
+   * array method called [].every() that returns a boolean and accepts a
+   * callabck that returns a boolean. It will iterate over each item and so long
+   * as everything comes back positive from the callback then the passed in set
+   * is a subset.
+   *
+   * @param {SetDS} set A SetDS object to compare against.
+   * @return {boolean}
+   */
   isSubset = (set) => (
     // If every item in the subset returns true it is a subset.
     set.values.every((value) => (
@@ -91,10 +134,20 @@ export default class SetDS {
     ))
   )
 
+  /**
+   * Get the length of the set.
+   * @return {integer}
+   */
   length = () => (
     this.numberOfValues
   )
 
+  /**
+   * Determine if this values exists in our set.
+   *
+   * @param {mixed} value The value to test against
+   * @return {boolean}
+   */
   contains = (value) => (
     this.values.indexOf(value) !== -1
   )
