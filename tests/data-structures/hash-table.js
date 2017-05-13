fc4a4245fb1d98bcf00618aa69bdb4039900bc55
Andric Villanueva <contact@andricvillanueva.com>
2017-05-13 19:26:17 -0400

    Adding hash table test.

---
+++ tests/data-structures/hash-table.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-unused-expressions, no-console */
+import chai from 'chai'
+// import HashTableDS from '../../data-structures/hash-table'
+
+chai.should()
+
+describe('Hash Table Data Structure', () => {
+  it('add(1...5) and get the length() of the linked list.', (done) => {
+    // const hash = new HashTableDS()
+    // hash.add(1).add(2).add(3).add(4).add(5)
+    //
+    // // Assertions.
+    // hash.length().should.equal(5)
+    // hash.toArray().should.deep.equal([1, 2, 3, 4, 5])
+
+    done()
+  })
+})
+
+// const hashTable = new HashTableDS(3)
+// hashTable.add('first', 1)
+// hashTable.add('second', 2)
+// hashTable.add('third', 3)
+// hashTable.add('fourth', 4)
+// hashTable.add('fifth', 5)
+// hashTable.print() // => 2 4 1 3 5
+// console.log('length gives 5:', hashTable.length()) // => 5
+// console.log('search second gives 2:', hashTable.search('second')) // => 2
+// hashTable.remove('fourth')
+// hashTable.remove('first')
+// hashTable.print() // => 2 3 5
+// console.log('length gives 3:', hashTable.length()) // => 3
