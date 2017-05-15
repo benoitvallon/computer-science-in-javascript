b58308daeaaa2013be58838cc70885f695305be3
Andric Villanueva <contact@andricvillanueva.com>
2017-05-15 17:32:19 -0400

    Adding recusricve bubble sort

--- sorting/bubble.js
+++ sorting/bubble.js
@@ -1,4 +1,9 @@
-/* eslint-disable no-console, no-param-reassign */
+/**
+ * A traditional bubble sort.
+ *
+ * @param {array} array The array to sort.
+ * @return {array}
+ */
 export const bubbleSort = (array) => {
   // This is so that we don't modify the array coming in.
   const sortedArray = array
@@ -23,4 +28,30 @@ export const bubbleSort = (array) => {
   }
 
   return sortedArray
+}
+
+/**
+ * A recursive bubble sort.
+ *
+ * @param {array} array The array to sort.
+ * @param {int} count The size of the array.
+ * @return {array}
+ */
+export const bubbleSortRecursive = (array, count) => {
+  if (count === 1) {
+    return array
+  }
+
+  const sortedArray = array
+
+  for (let current = 0; current < count - 1; current++) {
+    const next = current + 1
+    if (array[next] < array[current]) {
+      const temp = array[current]
+      sortedArray[current] = array[next]
+      sortedArray[next] = temp
+    }
+  }
+
+  return bubbleSortRecursive(array, count - 1)
 }
