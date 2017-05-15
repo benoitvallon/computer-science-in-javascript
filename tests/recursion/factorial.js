05400c499236336ef3899885d7f64303ef5f187d
Andric Villanueva <contact@andricvillanueva.com>
2017-05-14 23:10:11 -0400

    Adding some recursion excersizes

--- tests/recursion/factorial.js
+++ tests/recursion/factorial.js
@@ -3,30 +3,28 @@ import { factorial, addition } from '../../recursion/math'
 
 chai.should()
 
-describe('Recursion', () => {
-  describe('Math', () => {
-    it('factorial(1) and factorial(10)', (done) => {
-      const factorial1 = factorial(1)
-      const factorial10 = factorial(10)
+describe('[Recursion] Math', () => {
+  it('factorial(1) and factorial(10)', (done) => {
+    const factorial1 = factorial(1)
+    const factorial10 = factorial(10)
 
-      // Assertions.
-      factorial1.should.equal(1)
-      factorial10.should.equal(3628800)
+    // Assertions.
+    factorial1.should.equal(1)
+    factorial10.should.equal(3628800)
 
-      done()
-    })
+    done()
+  })
 
-    it('addition(1) and addition(10)', (done) => {
-      const addition0 = addition(0)
-      const addition1 = addition(1)
-      const addition10 = addition(10)
+  it('addition(1) and addition(10)', (done) => {
+    const addition0 = addition(0)
+    const addition1 = addition(1)
+    const addition10 = addition(10)
 
-      // Assertions.
-      addition0.should.equal(0)
-      addition1.should.equal(1)
-      addition10.should.equal(55)
+    // Assertions.
+    addition0.should.equal(0)
+    addition1.should.equal(1)
+    addition10.should.equal(55)
 
-      done()
-    })
+    done()
   })
 })
