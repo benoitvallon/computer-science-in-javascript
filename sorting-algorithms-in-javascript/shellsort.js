3fcd28f7c9b9e2c785ddaf54dca33fc04530474e
Rohit Paul <113459757+RohitPaul12345@users.noreply.github.com>
2023-08-07 10:24:59 +0530

    Update shellsort.js

--- sorting-algorithms-in-javascript/shellsort.js
+++ sorting-algorithms-in-javascript/shellsort.js
@@ -1,15 +1,15 @@
 // array to sort
-var array = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+let array = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
 
 // gaps
-var gaps = [701, 301, 132, 57, 23, 10, 4, 1];
+let gaps = [701, 301, 132, 57, 23, 10, 4, 1];
 
 function shellsort(array) {
-  for(var g = 0; g < gaps.length; g++) {
-    var gap = gaps[g];
-    for(var i = gap; i < array.length; i++) {
-      var temp = array[i];
-      for(var j = i; j >= gap && array[j - gap] > temp; j -= gap) {
+  for(let g = 0; g < gaps.length; g++) {
+    let gap = gaps[g];
+    for(let i = gap; i < array.length; i++) {
+      let temp = array[i];
+      for(let j = i; j >= gap && array[j - gap] > temp; j -= gap) {
         array[j] = array[j - gap];
       }
       array[j] = temp;
