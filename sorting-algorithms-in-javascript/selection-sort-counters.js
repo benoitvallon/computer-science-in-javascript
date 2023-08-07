639673b480f1c6d5bfb6a7f0fd8026f13408704a
Rohit Paul <113459757+RohitPaul12345@users.noreply.github.com>
2023-08-07 10:54:32 +0530

    Update selection-sort-counters.js

--- sorting-algorithms-in-javascript/selection-sort-counters.js
+++ sorting-algorithms-in-javascript/selection-sort-counters.js
@@ -1,24 +1,24 @@
 // sample of arrays to sort
-var arrayRandom = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
-var arrayOrdered = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-var arrayReversed = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+let arrayRandom = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+let arrayOrdered = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let arrayReversed = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
 
 // swap function helper
 function swap(array, i, j) {
-  var temp = array[i];
+  let temp = array[i];
   array[i] = array[j];
   array[j] = temp;
 }
 
 function selectionSort(array) {
-  var countOuter = 0;
-  var countInner = 0;
-  var countSwap = 0;
+  let countOuter = 0;
+  let countInner = 0;
+  let countSwap = 0;
 
-  for(var i = 0; i < array.length; i++) {
+  for(let i = 0; i < array.length; i++) {
     countOuter++;
-    var min = i;
-    for(var j = i + 1; j < array.length; j++) {
+    let min = i;
+    for(let j = i + 1; j < array.length; j++) {
       countInner++;
       if(array[j] < array[min]) {
         min = j;
